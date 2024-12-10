e97d31a4202fd1698169507e46a82b3468949ba9
prdimitrov <prdimitrov97@gmail.com>
2024-12-10 20:24:43 +0200

    Initial commit - Add two buttons to start and stop services - Add textView - Initialize the widgets in MainActivity.java class.

---
+++ app/src/main/java/com/example/musicplayerapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.musicplayerapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+TextView txt;
+Button startButton, stopButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+            startButton = findViewById(R.id.startButton);
+            stopButton = findViewById(R.id.stopButton);
+
+            return insets;
+        });
+    }
+}
