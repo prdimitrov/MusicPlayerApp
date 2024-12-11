ba19a78e40237f577c6e64b9039fe658f8e82788
prdimitrov <prdimitrov97@gmail.com>
2024-12-11 09:47:58 +0200

    Fix problem with sound not playing when service is started - Enable service in AndroidManifest.xml file - Add logging in onStartCommand method in MyCustomService.java class, also use .start() method for the MediaPlayer.

--- app/src/main/java/com/example/musicplayerapp/MyCustomService.java
+++ app/src/main/java/com/example/musicplayerapp/MyCustomService.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.provider.Settings;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -18,12 +20,25 @@ public class MyCustomService extends Service {
         //This will play the audio of default ringtone in the device
         player = MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI);
 
+        if (player == null) {
+            // Handle the case where MediaPlayer creation failed
+            Log.e("MyCustomService", "Failed to initialize MediaPlayer. Default ringtone not found.");
+            stopSelf();
+            return START_NOT_STICKY;
+        }
+
+        Log.d("MyCustomService", "MediaPlayer initialized successfully.");
+
         //Play the ringtone audio on loop (continuously)
         player.setLooping(true);
+        player.start();
+
+        Log.d("MyCustomService", "MediaPlayer started playing the ringtone.");
 
         //START_NOT_STICKY can be used for running services that
         //perform specific tasks and don't need to be running continuously!
 //        return START_NOT_STICKY;
+        Toast.makeText(this, "Playing ringtone...", Toast.LENGTH_SHORT).show();
         return START_STICKY;
     }
 
